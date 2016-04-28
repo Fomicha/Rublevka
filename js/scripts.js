f9b7771597bf42d54dd8900c95c6e470661a28b0
Fomi4 <fomich.aleksandr@gmail.com>
2016-04-28 19:11:08 +0300

    selectes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
-$('.select').on('click','.placeholder',function(){
-  var parent = $(this).closest('.select');
-  if ( ! parent.hasClass('is-open')){
-    parent.addClass('is-open');
-    $('.select.is-open').not(parent).removeClass('is-open');
-  }else{
-    parent.removeClass('is-open');
-  }
-}).on('click','ul>li',function(){
-  var parent = $(this).closest('.select');
-  parent.removeClass('is-open').find('.placeholder').text( $(this).text() );
+$(function(){
+  $('body').click(function() {
+    $('.select1').removeClass('is-open');
+  });
+
+  $('.select1').on('click','.placeholder1',function(e){
+    e.stopPropagation();
+        var parent = $(this).closest('.select1');
+    if (!parent.hasClass('is-open')){
+      parent.addClass('is-open');
+      $('.select.is-open').not(parent).removeClass('is-open');
+    }else{
+      parent.removeClass('is-open');
+    }
+  }).on('click','ul>li',function(){
+    var parent = $(this).closest('.select1');
+    parent.removeClass('is-open').find('.placeholder1').text( $(this).text() );
+    parent.find('input[type=hidden]').attr('value', $(this).attr('data-value') );
+  });
 });
